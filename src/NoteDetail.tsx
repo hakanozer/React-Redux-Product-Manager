870c2a6ffa36ea50a1dc5451d6fd51c2121adab7
Hakan ÖZER <hakanozer02@gmail.com>
2022-07-03 18:19:46 +0300

    Redux Push

--- src/NoteDetail.tsx
+++ src/NoteDetail.tsx
@@ -1,43 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import { StateType } from './useRedux/AppStore'
+import { ENote } from './useRedux/ENote'
+import { INoteAction } from './useRedux/INoteAction'
 
 function NoteDetail() {
 
+    const navigate = useNavigate()
     const { index } = useParams()
-    let indexInt = 0
+    let indexInt = -1
     if ( index ) {
         indexInt = parseInt(index)
     }
+
+
     // use redux
     const dispatch = useDispatch()
     const noteArr = useSelector( ( item: StateType ) => item.NoteReducer )
     const item = noteArr[indexInt]
+
+  const fncDelete = () => {
+    const answer = window.confirm("Are you sure Delete!, " + item.title )
+    if ( answer ) {
+        const deleteItem: INoteAction = {
+            type: ENote.NOTE_DELETE,
+            payload: item
+        }
+        dispatch(deleteItem)
+        navigate('/settings')
+    }
+  }  
+
+
+  useEffect(() => {
+    if ( !item ) {
+        navigate('/settings')
+    }
+  }, [])
+
+
   return (
     <>
-        <Helmet>
-            <title> Note Module </title>
-            <meta name="description" content="Note Module Page" />
-        </Helmet>
         <NavBar/>
+        <h2> Note Detail </h2>
         { item && 
-        <div className="card col-6">
-            <div className="card-body">
-                <h5 className="card-title"> { item.title } </h5>
-                <p className="card-text"> { item.detail } </p>
+        <>
+            <Helmet>
+                <title> { item.title } </title>
+                <meta name="description" content={ item.detail } />
+            </Helmet>
+            <div className="card col-6">
+                <div className="card-body">
+                    <h5 className="card-title"> { item.title } </h5>
+                    <p className="card-text"> { item.detail } </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><span className="badge text-bg-primary">ID:</span> { item.id }</li>
+                    <li className="list-group-item"> <span className="badge text-bg-primary">Date:</span> { item.date }</li>
+                </ul>
+                <div className="card-body">
+                <button onClick={(evt) => fncDelete() } className='btn btn-danger btn-sm'><i className="bi bi-trash3"></i></button>
+                </div>
             </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item"> { item.id }</li>
-                <li className="list-group-item"> { item.date }</li>
-            </ul>
-            <div className="card-body">
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-            </div>
-        </div>
+        </>
         }
     </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -36,7 +36,7 @@ function NavBar() {
     <>
        <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <NavLink to='/dashboard' className='navbar-brand' >Navbar</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
